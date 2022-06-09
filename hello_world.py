faf8ef38406b3aff82ee028adf649d63d5ca2a56
Rafael Auada <auadarafael@gmail.com>
2022-06-09 17:02:37 -0300

    Initialize python works

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+# print("Hello Python world!")
+
+# name = input("What is your name?\n")
+# print("Hi, %s." % name)
+
+message = "Hello Python world!"
+print(message)
+
+message = "Hello Python Crash Course world!"
+print(message)
