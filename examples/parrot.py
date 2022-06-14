f6db1a71029c24ea70044d84866b1ab3e3c18d68
Rafael Auada <auadarafael@gmail.com>
2022-06-14 11:53:38 -0300

    added new input from user example

---
+++ examples/parrot.py
@@ -0,0 +1,17 @@
+# message = input("Tell me something, and I will repeat it back to you:")
+# print(message)
+
+# name = input("Please enter your name: ")
+# print(f"\nHello, {name}")
+
+# age = input("How old are you?")
+# age = int(age)
+# print(age >= 18)
+
+height = input("How tall are you, in centimeters?")
+height = int(height)
+
+if height >= 120:
+	print("\nYou're tall enough to ride!")
+else:
+	print("\nYou'll be able to ride when you're a little older.")
